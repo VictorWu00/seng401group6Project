22f86a746367ed0b75f78258609c20ff32f8ea12
VictorWu00 <wusgod2299@gmail.com>
2022-03-04 08:32:20 -0700

    change

--- Seng 401 Projrct draft/src/main/java/com/ucalgary/librarySystem/controller/adminloginController.java
+++ Seng 401 Projrct draft/src/main/java/com/ucalgary/librarySystem/controller/adminloginController.java
@@ -33,7 +33,44 @@ public class adminloginController {
         else{
             return "error";
         }
-
     }
+
+    @RequestMapping("/admin")
+    public String jumpToAdmin(){
+        return "admin";
+    }
+
+    @RequestMapping("/AddBook")
+    public String jumpToAddBook(){
+        return "AddBook";
+    }
+
+    @RequestMapping("/AddB")
+    public String jumpToAdd(Model model, @RequestParam(name = "a", required = false) int isbn, @RequestParam(name = "b", required = false) String bname, @RequestParam(name = "c", required = false) String category, @RequestParam(name = "d", required = false) int year, @RequestParam(name = "e", required = false) String author, @RequestParam(name = "f", required = false) String publisher, @RequestParam(name = "g", required = false) String section, @RequestParam(name = "h", required = false) int location, @RequestParam(name = "i", required = false) String description){
+        /*System.out.print(isbn);
+        System.out.print(bname);
+        System.out.print(category);
+        System.out.print(year);
+        System.out.print(author);
+        System.out.print(publisher);
+        System.out.print(location);
+        System.out.print(section);
+        System.out.print(description);*/
+
+        dal.addBook(isbn, bname, description, category, year, author, publisher, section, location);
+        return "AddB";
+    }
+
+    @RequestMapping("/DeleteBook")
+    public String jumpToDeleteBook(){
+        return "DeleteBook";
+    }
+
+    @RequestMapping("/DeleteB")
+    public String jumpToDeleteB(Model model, @RequestParam(name = "name", required = false) String bName, @RequestParam(name = "author", required = false) String author){
+        dal.deleteBook(bName, author);
+        return "DeleteB";
+    }
+
 }
 
